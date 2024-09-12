aafb43ff7b776caad0ea83e63d79c41f65a8b53d
Simon Foucambert <simon.fcbrt@gmail.com>
2024-09-12 14:07:48 +0200

    modif script

--- tracker.py
+++ tracker.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-gC02_per_kwh = 3.7 # Source : Le Monde
-gC02_per_GWH = 3.7 * 1e6
+gCO2_per_kwh_nuclear = 3.7 # Source : Le Monde
+gCO2_per_GWH_nuclear = gCO2_per_kwh_nuclear * 1e6
 df_energy_produced = pd.read_csv("energy_produced.csv", sep=";", encoding='ISO-8859-1')
-AR_Tokyo_Paris = 1.457e3     # Source : alterna énergie
+kgCO2_AR_Tokyo_Paris = 1.457e3     # Source : alterna énergie
 
 
 print(df_energy_produced)
@@ -21,14 +21,14 @@ hydraulic_array = array_energy_produced[array_energy_produced[:,6]=="Hydraulic"]
 
 
 
-plt.figure("Hydraulic production vs years")
+plt.figure("Nuclear production vs years")
 plt.plot(np.int16(nuclear_array[:,0]), np.int32(nuclear_array[:,7]))
 plt.xlabel("Year")
 plt.ylabel("Energy production [GWh]")
 plt.show()
 
 
-plt.plot(np.int16(nuclear_array[:,0]),gC02_per_GWH*np.int32(nuclear_array[:,7]) )
+plt.plot(np.int16(nuclear_array[:,0]),gCO2_per_GWH_nuclear*np.int32(nuclear_array[:,7]) )
 plt.xlabel("Year")
 plt.ylabel("C02 of nuclear energy production")
 plt.show()
@@ -40,7 +40,7 @@ plt.ylabel("Energy production [GWh]")
 plt.show()
 
 
-plt.plot(np.int16(hydraulic_array[:,0]),gC02_per_GWH*np.int32(hydraulic_array[:,7]) )
+plt.plot(np.int16(hydraulic_array[:,0]),gCO2_per_GWH_nuclear*np.int32(hydraulic_array[:,7]) )
 plt.xlabel("Year")
 plt.ylabel("C02 of nuclear energy production")
 plt.show()
