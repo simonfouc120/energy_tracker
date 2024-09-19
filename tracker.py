4cfef36677202695a5608f1cea11ac432a34b8f5
Simonf <simon.fcbrt@gmail.com>
2024-09-19 08:20:04 -0700

    add calcul

--- tracker.py
+++ tracker.py
@@ -11,15 +11,10 @@ gCO2_per_GWH_hydraulic = gCO2_per_kwh_hydraulic * 1e6
 df_energy_produced = pd.read_csv("energy_produced.csv", sep=";", encoding='ISO-8859-1')  # Source : EDF
 kgCO2_AR_Tokyo_Paris = 1.457e3 * 2     # Source : impact CO2 transport
 
-
-
-
+gCO2_per_person_per_year = 8,9e6  # Source : statistique.developpement-durable.gouv.fr
 
 array_energy_produced = np.array(df_energy_produced)
-# print(array_energy_produced)
 
-
-# print(array_energy_produced[:,6])
 nuclear_array = array_energy_produced[array_energy_produced[:,6]=="Nuclear"]
 years_nuclear = np.int16(nuclear_array[:,0])
 produced_energy_nuclear = np.int32(nuclear_array[:,7])
@@ -28,15 +23,29 @@ hydraulic_array = array_energy_produced[array_energy_produced[:,6]=="Hydraulic"]
 years_hydraulic = np.int16(hydraulic_array[:,0])
 produced_energy_hydraulic = np.int32(hydraulic_array[:,7])
 
+thermical_array = array_energy_produced[array_energy_produced[:,6]=="Flame thermal"]
+years_thermical = np.int16(thermical_array[:,0])
+produced_energy_thermical = np.int32(thermical_array[:,7])
 
-# plt.plot(years_hydraulic)
-# plt.plot(years_nuclear, produced_energy_nuclear, marker='o')
-# plt.plot(years_hydraulic, produced_energy_hydraulic, marker='o')
-# plt.xlabel("Year")
-# plt.ylabel("Energy production [GWh]")
-# plt.show()
+plt.figure("Various energy production vs years")
 
-print(np.int32(np.max(produced_energy_nuclear)/kgCO2_AR_Tokyo_Paris*1e3))
+plt.plot(years_nuclear, produced_energy_nuclear, marker='x', label = " nuclear")
+plt.plot(years_hydraulic, produced_energy_hydraulic, marker='x', label = "hydraulic")
+plt.plot(years_thermical ,produced_energy_thermical,marker ='x', label = "thermical")
+plt.xlabel("Year")
+plt.ylabel("Energy production [GWh]")
+plt.legend()
+plt.show()
+
+
+ 
+
+print("Equivalent CO2 en nombre d'aller retour Paris - Tokyo pour une production maximale annuelle via le nucléaire : ",np.int32(np.max(produced_energy_nuclear)/kgCO2_AR_Tokyo_Paris*1e3))
+print("Equivalent CO2 en nombre d'aller retour Paris - Tokyo pour une même production annuelle mais via le nucléaire : ",np.int32(np.max(produced_energy_nuclear)/kgCO2_AR_Tokyo_Paris*1e3))
+
+
+
+
 
 plt.figure("Nuclear production vs years")
 plt.plot(years_nuclear, produced_energy_nuclear)
@@ -62,3 +71,6 @@ plt.plot(years_hydraulic,gCO2_per_GWH_hydraulic*produced_energy_hydraulic )
 plt.xlabel("Year")
 plt.ylabel("C02 of nuclear energy production")
 plt.show()
+
+
+
